8d75883823ba5ebb51b7c9627ff823987694b4bb
martinna13 <ry1841155@gmail.com>
2025-03-10 12:33:07 +0700

    Pesan commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container mt-4">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Profil Saya</h1>
+                </div>
+                <div class="card-body">
+                    <img src="p.jpg" alt="Profile Picture" width="150">
+                    <p>Martina Tri Rahmayanti</p>
+                    <p>Email  : ry1841155@gmail.com</p>
+                    <p>Bio    : Penjahatmu </p>
+                    <p>Skills : Baperin Orang </p>
+                    <ul>
+                        <li>Makan</li>
+                        <li>Tidur</li>
+                        <li>Main</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
